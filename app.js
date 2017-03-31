84703cc8a446d742702114bfedbf5cb14be265d7
Jeremy Taylor <goldplatedmarketer@gmail.com>
2017-03-30 20:25:59 -0700

    created getYoutubeData function that is used for api

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 //api key: AIzaSyBFXICcSjWGxBOlHQgcPfkOqgEChPGEgDc
+//end point link: https://www.googleapis.com/youtube/v3/search
+
+var YT_BASE_URL = 'https://www.googleapis.com/youtube/v3/search'
+
+function getYoutubeData(searchTerm, callback) {
+    var query = {
+        //properties
+    }
+}
