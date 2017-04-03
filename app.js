9c2f0c2a0302df0f465de4b7c2cc77fdf831acdb
Jeremy Taylor <goldplatedmarketer@gmail.com>
2017-04-02 23:16:27 -0700

    added youtube params for get yt data function

--- app.js
+++ app.js
@@ -3,8 +3,32 @@
 
 var YT_BASE_URL = 'https://www.googleapis.com/youtube/v3/search'
 
+//retrieve api function
 function getYoutubeData(searchTerm, callback) {
-    var query = {
-        //properties
+    var params = {
+        part: 'snippet',
+        key: 'AIzaSyBFXICcSjWGxBOlHQgcPfkOqgEChPGEgDc',
+        q: 'searchTerm'
+
     }
 }
+
+//render api data function
+
+var dataResultElem = '';
+
+function renderData(data) {
+    if (data.Search) {
+        data.Search.forEach(function (i) {
+
+        })
+    }
+
+
+}
+
+
+//event listener for api
+
+
+//callback on load for event listener
